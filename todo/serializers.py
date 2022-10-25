43e426f14214178ce2b9c5b81a99a280470acebb
NIK <nikita1z@mail.ru>
2022-10-25 20:59:59 +0300

    Создал сеарилизатор и модель.

---
+++ todo/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    '''Сереализовываем нашу модель в json представление'''
+
+    class Meta:
+        model = Todo
+        fields = '__all__'
