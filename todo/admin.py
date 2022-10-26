1cf3d46e8c5cce7286173acc574c8c7e0561445f
NIK <nikita1z@mail.ru>
2022-10-26 19:55:02 +0300

    Проверил CRUD

--- todo/admin.py
+++ todo/admin.py
@@ -7,5 +7,8 @@ class TodoAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'date', 'done')  # Чтов идим в спимке
     list_display_links = ('id', 'title')  #
     search_fields = ('id', 'title', 'description')  # По каким полям можем искать
-    list_editable = ('done')  # Что можем изменить прямо в списке
-    list_filter = ('done')  # По каким параметрам можем фильтровать
+    list_editable = ('done',)  # Что можем изменить прямо в списке
+    list_filter = ('done',)  # По каким параметрам можем фильтровать
+
+
+admin.site.register(Todo, TodoAdmin)
